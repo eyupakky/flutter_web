f260b4fe9620bd47fd60047a0cd5e1511c132a4e
eyup.akky <2255629e>
2021-09-17 17:08:13 +0300

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,158 @@
+import 'dart:html' as html;
+import 'dart:js' as js;
+import 'dart:ui' as ui;
+
 import 'package:flutter/material.dart';
+import 'package:iddaaapp/social_button.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Davide Agostini Hub',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  String socialName = "@davideagostini";
+  String description =
+      "Mobile Software Engineer (Android/Flutter) \nwho loves create interesting mobile apps with an eye to UI.";
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      backgroundColor: Color(0xFFFEFEFE),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(
+                  height: 40,
+                ),
+                // Container(
+                //   width: 150,
+                //   height: 150,
+                //   decoration: BoxDecoration(
+                //     shape: BoxShape.circle,
+                //     color: Colors.white,
+                //     image: DecorationImage(
+                //       fit: BoxFit.fill,
+                //       image: AssetImage('assets/davide.jpg'),
+                //     ),
+                //     boxShadow: [
+                //       BoxShadow(
+                //         color: Colors.black12,
+                //         offset: Offset(0, 4),
+                //         blurRadius: 4.0,
+                //       )
+                //     ],
+                //   ),
+                // ),
+                const SizedBox(
+                  height: 25,
+                ),
+                Text(
+                  socialName,
+                  style: TextStyle(
+                    fontSize: 28,
+                    color: Colors.black87,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                Text(
+                  description,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                      height: 1.5,
+                      letterSpacing: 0),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Text(
+                  'Social links',
+                  style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black87),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Container(
+                  width: 150,
+                  height: 1,
+                  color: Colors.grey[300],
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                SocialButton(
+                  url: 'https://davideagostini.medium.com',
+                  icon: MdiIcons.typewriter,
+                  iconColor: Colors.black,
+                  label: "Medium blog",key: UniqueKey()
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                SocialButton(
+                  url: 'https://github.com/davideagostini',
+                  icon: MdiIcons.github,
+                  iconColor: Colors.black,
+                  label: "GitHub profile",key: UniqueKey()
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                SocialButton(
+                  url: 'https://www.linkedin.com/in/davideagostini/',
+                  icon: MdiIcons.linkedin,
+                  iconColor: Colors.blue,
+                  label: "LinkedIn profile",key: UniqueKey()
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                SocialButton(
+                  url: 'https://twitter.com/davideagostini',
+                  icon: MdiIcons.twitter,
+                  iconColor: Colors.lightBlue,
+                  label: "I'm on Twitter!", key: UniqueKey(),
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
